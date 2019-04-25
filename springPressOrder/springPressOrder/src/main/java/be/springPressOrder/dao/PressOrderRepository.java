89c122e7780c5170725de5e53a66f45740243ea7
alexandra.klochkova@student.odisee.be <alexandra.klochkova@student.odisee.be>
2019-04-25 16:16:06 +0200

    toegang tot pressOrders + Orders met admin

--- springPressOrder/springPressOrder/src/main/java/be/springPressOrder/dao/PressOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package be.springPressOrder.dao;
-
-import be.springPressOrder.domain.PressOrder;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PressOrderRepository extends CrudRepository<PressOrder, Integer> {
-}
