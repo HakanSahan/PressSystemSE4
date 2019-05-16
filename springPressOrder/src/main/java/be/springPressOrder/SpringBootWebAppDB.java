31dfe88dd1fa5030133c055aec7bbdef9392f856
HakanSahan <32307236+HakanSahan@users.noreply.github.com>
2019-05-16 13:52:55 +0200

    ll

--- springPressOrder/src/main/java/be/springPressOrder/SpringBootWebAppDB.java
+++ springPressOrder/src/main/java/be/springPressOrder/SpringBootWebAppDB.java
@@ -39,41 +39,41 @@ public class SpringBootWebAppDB implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         List<Order> orders = Arrays.asList(
-        new Order(2,"AppleJuice",1),
-        new Order(3,"PearJuice",2),
-        new Order(4,"ApplePearJuice",3));
+                new Order(),
+                new Order(),
+                new Order());
 
-        for (Order order: orders) {
+        for (Order order : orders) {
             orderRepository.save(order);
         }
 
         //orderRepository.save(order1);
         //log.info("Saved order1 - id: " + order1.getId());
 
-        Order order2 = new Order(3,"PearJuice",2);
+        Order order2 = new Order();
         orderRepository.save(order2);
         //log.info("Saved order2 - id: " + order2.getId());
-        for(Order order:orders){
+        for (Order order : orders) {
             List<PressOrder> pressOrders = Arrays.asList(
-                    new PressOrder(100, 99, 100,order),
-                    new PressOrder(1, 7, 1 ,order),
-                    new PressOrder(2, 6, 2,order));
+                    new PressOrder(100, 99, order),
+                    new PressOrder(1, 7,  order),
+                    new PressOrder(2, 6, order));
 
-            for (PressOrder pressOrder: pressOrders) {
+            for (PressOrder pressOrder : pressOrders) {
                 pressOrderRepository.save(pressOrder);
+            }
 
+            PressOrder pressOrder1 = new PressOrder(100, 99, order2);
+            pressOrderRepository.save(pressOrder1);
+            //log.info("Saved press order1 - id: " + pressOrder1.getId());
+
+            PressOrder pressOrder2 = new PressOrder(1, 7,  order2);
+            pressOrderRepository.save(pressOrder2);
+            //log.info("Saved press order2 - id: " + pressOrder2.getId());
+
+            PressOrder order3 = new PressOrder(2, 6, order2);
+            pressOrderRepository.save(order3);
+            //log.info("Saved press order3 - id: " + order3.getId());
         }
-
-        PressOrder pressOrder1 = new PressOrder(100, 99, 100, PressOrder.Status.Planned,order2);
-        pressOrderRepository.save(pressOrder1);
-        log.info("Saved press order1 - id: " + pressOrder1.getId());
-
-        PressOrder pressOrder2 = new PressOrder(1, 7, 1, PressOrder.Status.NotPlanned,order2);
-        pressOrderRepository.save(pressOrder2);
-        log.info("Saved press order2 - id: " + pressOrder2.getId());
-
-        PressOrder order3 = new PressOrder(2, 6, 2, PressOrder.Status.NotPlanned,order1);
-        pressOrderRepository.save(order3);
-        log.info("Saved press order3 - id: " + order3.getId());
     }
 }
