dab6727b16b0cb3908e2332d8ebf36308e8b37a1
Elliot <elliot.borryn@student.odisee.be>
2019-05-17 14:59:16 +0200

    Map Change

--- src/main/java/be/springPressOrder/dao/MachineRepository.java
+++
@@ -1,9 +0,0 @@
-package be.springPressOrder.dao;
-
-import be.springPressOrder.domain.Machine;
-import be.springPressOrder.domain.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MachineRepository extends CrudRepository<Machine, Integer> {
-
-}
--- src/main/java/be/springPressOrder/dao/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package be.springPressOrder.dao;
-
-import be.springPressOrder.domain.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Integer> {
-
-}
