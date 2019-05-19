b7df74f7a9d1a7b18bfc3a1c9be80d2df5717d28
HakanSahan <32307236+HakanSahan@users.noreply.github.com>
2019-05-19 15:08:58 +0200

    kleine fix

--- springPressOrder/src/main/java/be/springPressOrder/SpringBootWebAppDB.java
+++ springPressOrder/src/main/java/be/springPressOrder/SpringBootWebAppDB.java
@@ -7,11 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Logger;
 
 
 @Component
@@ -27,24 +25,6 @@ public class SpringBootWebAppDB implements ApplicationListener<ContextRefreshedE
     private UserRepository userRepository;
 
     @Autowired
-    public void setMachineRepository(MachineRepository machineRepository){this.machineRepository = machineRepository;}
-
-        User user = new User(1,"admin","{noop}password","ROLE_ADMIN","a","a","a");
-
-        userRepository.save(user);
-
-        Fruit fruit = new Fruit("Apple");
-        fruitRepository.save(fruit);
-
-        machineRepository.save(new Machine());
-
-      List<Order> orders = Arrays.asList(
-                new Order(50,fruit,1),
-                new Order(50,fruit,1),
-                new Order(50,fruit,1));
-
-            orderRepository.save(orders);
-    @Autowired
     public void setScheduleRepository(ScheduleRepository scheduleRepository){this.scheduleRepository = scheduleRepository;}
 
     @Autowired
@@ -53,60 +33,62 @@ public class SpringBootWebAppDB implements ApplicationListener<ContextRefreshedE
     @Autowired
     public void setRequestTechnicianRepository(RequestTechnicianRepository requestTechnicianRepository){this.requestTechnicianRepository = requestTechnicianRepository;}
 
-        Order order2 = new Order(50,fruit,1);
-        orderRepository.save(order2);
-        //log.info("Saved order2 - id: " + order2.getId());
-
-          List<PressOrder> pressOrders = Arrays.asList(
-                    new PressOrder(100, 99, order2),
-                    new PressOrder(1, 7,  order2),
-                    new PressOrder(2, 6, order2));
-
-                pressOrderRepository.save(pressOrders);
-
-
-            PressOrder pressOrder1 = new PressOrder(100, 99, order2);
-            pressOrderRepository.save(pressOrder1);
-            //log.info("Saved press order1 - id: " + pressOrder1.getId());
-
-            PressOrder pressOrder2 = new PressOrder(1, 7,  order2);
-            pressOrderRepository.save(pressOrder2);
-            //log.info("Saved press order2 - id: " + pressOrder2.getId());
-
-            PressOrder order3 = new PressOrder(2, 6, order2);
-            pressOrderRepository.save(order3);
-            //log.info("Saved press order3 - id: " + order3.getId());
-        }
-    }
     @Autowired
     public void setUserRepository(UserRepository userRepository){this.userRepository = userRepository;}
+
+    @Autowired
+    public void setMachineRepository(MachineRepository machineRepository){this.machineRepository = machineRepository;}
+
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
-        Fruit apple = new Fruit("Apple",0.1,5,7);
-        Fruit pear = new Fruit("Pear",0.2,2,3);
+        User user = new User("admin","admin","047","ROLE_ADMIN","admin","ROLE_ADMIN","{noop}password");
+
+        userRepository.save(user);
+
+        Fruit fruit = new Fruit("Apple");
+        fruitRepository.save(fruit);
+
+        machineRepository.save(new Machine());
+
+        List<Order> orders = Arrays.asList(
+                new Order(50,fruit,1),
+                new Order(50,fruit,1),
+                new Order(50,fruit,1));
+
+        orderRepository.save(orders);
+
+        Order order2 = new Order(50,fruit,1);
+        orderRepository.save(order2);
+        //log.info("Saved order2 - id: " + order2.getId());
+
+        List<PressOrder> pressOrders = Arrays.asList(
+                new PressOrder(100, 99, order2),
+                new PressOrder(1, 7,  order2),
+                new PressOrder(2, 6, order2));
+
+        pressOrderRepository.save(pressOrders);
+
+
+        PressOrder pressOrder1 = new PressOrder(100, 99, order2);
+        pressOrderRepository.save(pressOrder1);
+        //log.info("Saved press order1 - id: " + pressOrder1.getId());
+
+        PressOrder pressOrder2 = new PressOrder(1, 7,  order2);
+        pressOrderRepository.save(pressOrder2);
+        //log.info("Saved press order2 - id: " + pressOrder2.getId());
+
+        PressOrder order3 = new PressOrder(2, 6, order2);
+        pressOrderRepository.save(order3);
+        //log.info("Saved press order3 - id: " + order3.getId());
+        Fruit apple = new Fruit("Apple");
+        Fruit pear = new Fruit("Pear");
         fruitRepository.save(apple);
         fruitRepository.save(pear);
         Order order1 = new Order(2,apple,1);
         orderRepository.save(order1);
-        log.info("Saved order1 - id: " + order1.getId());
-
-        Order order2 = new Order(3,pear,2);
-        orderRepository.save(order2);
-        log.info("Saved order2 - id: " + order2.getId());
-
-        PressOrder pressOrder1 = new PressOrder(100,10,new Order(2,pear,1));
-        pressOrderRepository.save(pressOrder1);
-        log.info("Saved press order1 - id: " + pressOrder1.getId());
-
-        PressOrder pressOrder2 = new PressOrder(100,10,new Order(2,apple,1));
-        pressOrderRepository.save(pressOrder2);
-        log.info("Saved press order2 - id: " + pressOrder2.getId());
-
-        PressOrder order3 = new PressOrder(100,10,new Order(2,apple,1));
-        pressOrderRepository.save(order3);
-        log.info("Saved press order3 - id: " + order3.getId());
+        ///log.info("Saved order1 - id: " + order1.getId());
 
         Machine machine1 = new Machine(1,false,100);
         Machine machine2 = new Machine(2,false,100);
@@ -143,9 +125,9 @@ public class SpringBootWebAppDB implements ApplicationListener<ContextRefreshedE
 
         scheduleRepository.save(schedule);
 
-        User user = new User("Vanvolsem","Pierre","03","p@v.com","pv","PRESSER",
+        User user5 = new User("Vanvolsem","Pierre","03","p@v.com","pv","PRESSER",
                 "presser");
-        userRepository.save(user);
+        userRepository.save(user5);
 
         User user2 = new User("Sahan","Hakan","04","h@s.com","hs","ADMIN",
                 "admin");
@@ -154,24 +136,5 @@ public class SpringBootWebAppDB implements ApplicationListener<ContextRefreshedE
         User user3 = new User("Klochkova","Alex","05","a@k.com","ak","TECHNICIAN",
                 "tech");
         userRepository.save(user3);
-
-        /*PressOrder shirt = new PressOrder();
-        shirt.setDescription("Spring Framework Guru Shirt");
-        shirt.setPrice(new BigDecimal("18.95"));
-        shirt.setImageUrl("https://springPressOrder.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
-        shirt.setProductId("235268845711068308");
-        pressOrderRepository.save(shirt);
-
-        log.info("Saved Shirt - id: " + shirt.getId());
-
-        PressOrder mug = new PressOrder();
-        mug.setDescription("Spring Framework Guru Mug");
-        mug.setImageUrl("https://springPressOrder.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
-        mug.setProductId("168639393495335947");
-        mug.setPrice(new BigDecimal("11.95"));
-        pressOrderRepository.save(mug);
-
-        log.info("Saved Mug - id:" + mug.getId());
-        */
     }
 }
