41a244f2c16df34b0aeb992ffa82a5c266e4ca7d
alexandra.klochkova@student.odisee.be <alexandra.klochkova@student.odisee.be>
2019-05-19 16:07:33 +0200

    Update SecurityConfiguration.java

--- springPressOrder/src/main/java/be/springPressOrder/SecurityConfiguration.java
+++ springPressOrder/src/main/java/be/springPressOrder/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       /* http.authorizeRequests()
+       http.authorizeRequests()
 
                 .antMatchers("/favicon.ico").permitAll()
                 .antMatchers("/login*").permitAll()
@@ -39,8 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/orders/**").hasAnyRole("ADMIN")
                 .antMatchers("/schedule").permitAll()
                 .antMatchers(HttpMethod.POST, "/schedule/test").permitAll()
-                .antMatchers("/request").hasAnyRole("USER","ADMIN")
-                .antMatchers("/request/**").hasAnyRole("USER","ADMIN")
+                .antMatchers("/request").hasAnyRole("PRESSER","ADMIN")
+                .antMatchers("/request/**").hasAnyRole("PRESSER","ADMIN")
                 //.antMatchers("/order/**").hasAnyRole("ADMIN")
                 .antMatchers("/pressorders/**").hasAnyRole("USER","ADMIN","TECHNICIAN")
                 .antMatchers("/pressorder/**").hasAnyRole("USER","ADMIN")
@@ -49,7 +49,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .formLogin().loginPage("/login").failureUrl("/login-error")
                         .defaultSuccessUrl("/menu",true).permitAll().and()
                     .logout().invalidateHttpSession(true).logoutSuccessUrl("/logout").permitAll();
-        http.exceptionHandling().accessDeniedPage("/403");*/
+        http.exceptionHandling().accessDeniedPage("/403");
        http.authorizeRequests().anyRequest().permitAll();
         http.csrf().and().cors().disable();
         http.headers().frameOptions().disable();
