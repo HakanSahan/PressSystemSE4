82ce389cda8182aed5f2533579e4a1bd6e5db3f4
Elliot <elliot.borryn@student.odisee.be>
2019-05-21 21:33:07 +0200

    post request test

--- springPressOrder/src/main/java/be/springPressOrder/dao/OrderRepository.java
+++ springPressOrder/src/main/java/be/springPressOrder/dao/OrderRepository.java
@@ -1,18 +1,8 @@
 package be.springPressOrder.dao;
 
 import be.springPressOrder.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+public interface OrderRepository extends CrudRepository<Order, Integer> {
 
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-    List<Order> findByIdClient(int idClient);
-
-    public Order findById(int id);
-
-    //public Order findOrderByUser(User user);
-
-    //public List<Order> findAllByUserOrderByName(User user);
-
-    //void delete(Integer id);
 }
